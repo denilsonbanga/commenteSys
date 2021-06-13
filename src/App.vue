d0a5fd4895cbe2d0bfd466da25430889fb096886
denilsonbanga <denilsonbanga13@gmail.com>
2021-06-13 23:39:10 +0100

    terminado

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <comments/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import comments from './components/Comments'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    comments
   }
 }
 </script>
---
+++ src/components/Comments.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app" class="container">
+    <h1>Comment System</h1>
+    <hr />
+    <div class="form-todo form-group">
+      <p>
+        <input
+          placeholder="nome"
+          type="text"
+          name="author"
+          class="form-control"
+          v-model="autor"
+        />
+      </p>
+      <p>
+        <textarea
+          placeholder="comentário"
+          name="messge"
+          class="form-control"
+          v-model="comentario"
+        ></textarea>
+      </p>
+      <button v-on:click="addComment" type="submit" class="btn-primary">
+        Comentar
+      </button>
+      <p v-if="comments.length <=0">Sem comentário</p>
+      <hr />
+    </div>
+    <div class="list-group">
+      <div class="list-group-item" v-for="(comment, index) in comments" :key="index">
+        <span class="comment_author"
+          >Autor: <strong>{{ comment.name }}</strong></span
+        >
+        <p>{{ comment.messege }}</p>
+        <div>
+          <button
+            typr="submit"
+            class="btn-danger"
+            v-on:click="reComment(index)"
+          >
+            Excluir
+          </button>
+        </div>
+      </div>
+    </div>
+    <hr>/
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      comments: [],
+      autor: "",
+      comentario: "",
+    };
+  },
+  methods: {
+    addComment() {
+      if (this.autor.trim() == "" || this.comentario.trim() == "") {
+        return;
+      }
+      this.comments.push({
+        name: this.autor,
+        messege: this.comentario,
+      });
+
+      this.autor = "";
+      this.comentario = "";
+    },
+    reComment(index) {
+      this.comments.pop(index);
+      //this.comments.splice(index,1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
